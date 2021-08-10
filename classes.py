24b70153d18eeb929e832930b26bf28284f5b2f4
Ben Hedgepeth <benhh@gmx.com>
2021-08-09 21:27:10 -0700

    added Hand class

--- classes.py
+++ classes.py
@@ -1,16 +1,79 @@
 
+from functools import reduce
+
 class Card:
 
     def __init__(self, suit, value):
         self.suit = suit
         if value not in ['Jack', 'Queen', 'King', 'Ace']:
-            self.pip_value = value
+            self.pip = value
         else:
             setattr(self, 'face_card', value)
-            self.pip_value = 11 if self.face_card == "Ace" else 10
+            self.pip = 11 if self.face_card == "Ace" else 10
 
     def __str__(self):
-        return f"{self.face_card} of {self.suit}"
+        if hasattr(self, 'face_card'):
+            return f"{self.face_card} of {self.suit}"
+        return f"{self.pip} of {self.suit}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.suit}, {self.face_card})"
+        if hasattr(self, 'face_card'):
+            return f"{self.__class__.__name__}({self.suit}, {self.face_card})"
+        return f"{self.__class__.__name__}({self.suit}, {self.pip})"
+
+
+class Hand:
+    def __init__(self, cards):
+        self.cards = cards
+        self.bust = False
+        self.soft = False
+        self.split = False
+        self.double_down = False
+
+    @property
+    def value(self):
+        _value = sum(card.pip for card in self.cards)
+        if _value > 21:
+            total_aces = filter(
+                lambda i: (
+                    hasattr(self.cards[i], 'face_card')
+                    and self.cards[i].face_card == "Ace"
+                ), range(len(self.cards)))
+            for ace_index in total_aces:
+                self.cards[ace_index].pip = 1
+                _value = sum(card.pip for card in self.cards)
+                if _value < 21:
+                    return _value
+        return _value
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+
+
+
+
+
+
+
+
+
+
+    def __str__(self):
+        return reduce(lambda y, z: f"{y}\n{z}", self.cards)
+
+    def __repr__(self):
+        cls = f"{self.__class__.__name__}(["
+        cls += ", ".join(
+            f"Card({card.suit}, {card.face_card})"
+            if hasattr(card, 'face_card')
+            else f"Card({card.suit}, {card.pip})"
+            for card in self.cards
+        )
+        return f"{cls}])"
+
+    def __gt__(self, other):
+        return self.value > other.value
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -17,26 +17,124 @@ class TestBlackjackFaceCard(TestCase):
 
         self.face_card = classes.Card("Hearts", "Ace")
 
-
     def test_face_card_value(self):
         for card_value in self.face_cards:
             with self.subTest(card_value=card_value):
                 suit, value = card_value
                 card = classes.Card(suit, value)
                 self.assertIsInstance(card, classes.Card)
-                self.assertEqual(card.pip_value, 10)
+                self.assertEqual(card.pip, 10)
 
     def test_ace_face_card_value(self):
         suit, value = self.ace_card
         card = classes.Card(suit, value)
         self.assertIsInstance(card, classes.Card)
-        self.assertEqual(card.pip_value, 11)
+        self.assertEqual(card.pip, 11)
 
     def test_face_card_string(self):
         self.assertEqual(str(self.face_card), "Ace of Hearts")
 
     def test_face_card_repr_string(self):
         self.assertEqual(repr(self.face_card), "Card(Hearts, Ace)")
+
+
+class TestBlackjackHandStrings(TestCase):
+    '''Verify that a blackjack hand has a str and repr string'''
+
+    def setUp(self):
+        self.hand1 = classes.Hand(
+            [classes.Card("Hearts", 'Jack'), classes.Card("Hearts", "Queen")]
+        )
+        self.hand2 = classes.Hand([
+            classes.Card("Clubs", 2), classes.Card("Diamonds", 9),
+            classes.Card("Clubs", 5)
+        ])
+        self.hand3 = classes.Hand([
+            classes.Card("Hearts", "Jack"), classes.Card("Hearts", 4)
+        ])
+
+    def test_blackjack_hand_string(self):
+
+        self.assertEqual(
+            str(self.hand1),
+            "Jack of Hearts\nQueen of Hearts"
+        )
+        self.assertEqual(
+            str(self.hand2),
+            "2 of Clubs\n9 of Diamonds\n5 of Clubs"
+        )
+
+    def test_blackjack_hand_repr_string(self):
+        self.assertEqual(
+            repr(self.hand2),
+            "Hand([Card(Clubs, 2), Card(Diamonds, 9), Card(Clubs, 5)])"
+        )
+        self.assertEqual(
+            repr(self.hand3),
+            "Hand([Card(Hearts, Jack), Card(Hearts, 4)])"
+        )
+
+
+class TestBlackjackSoftHandValue(TestCase):
+    '''Verify that the value of a soft blackjack hand is reduced
+    to prevent the hand from exceeding 21.'''
+
+    def setUp(self):
+        self.hand1 = classes.Hand([
+            classes.Card("Clubs", "Ace"), classes.Card("Diamonds", "Ace"),
+            classes.Card("Hearts", "Ace"), classes.Card("Spades", "Ace"),
+            classes.Card("Clubs", "Ace"), classes.Card("Diamonds", "Ace"),
+            classes.Card("Hearts", "Ace"), classes.Card("Spades", "Ace"),
+            classes.Card("Clubs", "Ace"), classes.Card("Diamonds", "Ace"),
+            classes.Card("Hearts", "Ace"), classes.Card("Spades", "Ace"),
+            classes.Card("Clubs", "Ace"), classes.Card("Diamonds", "Ace"),
+            classes.Card("Hearts", "Ace"), classes.Card("Spades", "Ace"),
+            classes.Card("Clubs", "Ace"), classes.Card("Diamonds", "Ace"),
+            classes.Card("Hearts", "Ace"), classes.Card("Spades", "Ace"),
+            classes.Card("Hearts", "Ace"), classes.Card("Spades", "Ace")
+        ])
+
+        self.hand2 = classes.Hand([
+            classes.Card("Hearts", 10), classes.Card("Hearts", 6),
+            classes.Card("Hearts", "Ace"), classes.Card("Clubs", "Ace")
+        ])
+
+        self.hand3 = classes.Hand([
+            classes.Card("Diamonds", 'Ace'), classes.Card("Spades", 2)
+        ])
+
+        self.hand_values = [22, 18, 13]
+
+    def test_soft_player_hands(self):
+        for i, hand in enumerate([self.hand1, self.hand2, self.hand3]):
+            with self.subTest(hand=hand, i=i):
+                self.assertEqual(hand.value, self.hand_values[i])
+
+
+class TestBlackjackHandComparisonOperators(TestCase):
+    '''Verify that one blackjack hand can be compared with another
+    blackjack hand.'''
+
+    def setUp(self):
+        self.hand1 = classes.Hand(
+            [classes.Card("Clubs", 10), classes.Card("Hearts", 4)]
+        )
+        self.hand2 = classes.Hand(
+            [classes.Card("Diamonds", "Ace"), classes.Card("Clubs", "Ace")]
+        )
+
+        self.hand3 = classes.Hand(
+            [classes.Card("Diamonds", 9), classes.Card("Clubs", 3)]
+        )
+
+    def test_blackjack_hand_greater_than(self):
+        self.assertTrue(self.hand1 > self.hand2)
+
+    def test_blackjack_hands_equal(self):
+        self.assertTrue(self.hand2 == self.hand3)
+
+
+
 
 
 if __name__ == "__main__":
